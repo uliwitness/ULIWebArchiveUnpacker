2dbe2f540625b775b531120f533b32c119879dbc
Uli Kusterer <ulivc@zathras.de>
2017-05-06 05:08:06 +0200

    Code that reads a web archive as a plist and tries to generate sensibe file names from the files and determine good file extensions for them.

--- ULIWebArchiveUnpacker/AppDelegate.m
+++ ULIWebArchiveUnpacker/AppDelegate.m
@@ -24,4 +24,47 @@
 }
 
 
+-(BOOL) createFileForResource: (NSDictionary*)dict basePath: (NSString*)basePath
+{
+	NSData * fileData = dict[@"WebResourceData"];
+	NSString * fileName = [dict[@"WebResourceURL"] lastPathComponent];
+	NSRange cgiParamOffset = [fileName rangeOfString: @"?"];
+	if( cgiParamOffset.location != NSNotFound )
+		fileName = [fileName substringToIndex: cgiParamOffset.location];
+	fileName = [basePath stringByAppendingPathComponent: fileName];
+	
+	NSString * utiForMimeType = CFBridgingRelease(UTTypeCreatePreferredIdentifierForTag( kUTTagClassMIMEType, (__bridge CFStringRef)dict[@"WebResourceMIMEType"], NULL ));
+	NSString * filenameExtension = CFBridgingRelease(UTTypeCopyPreferredTagWithClass( (__bridge CFStringRef)utiForMimeType, kUTTagClassFilenameExtension ));
+	if( filenameExtension && ![fileName hasSuffix: filenameExtension] )
+		fileName = [fileName stringByAppendingFormat: @".%@", filenameExtension];
+	
+	return [fileData writeToFile: fileName atomically: YES];
+}
+
+
+-(BOOL) application:(NSApplication *)sender openFile:(NSString *)filename
+{
+	NSDictionary * dict = [NSDictionary dictionaryWithContentsOfFile: filename];
+	if( !dict )
+		return NO;
+	
+	NSString * basePath = [filename stringByDeletingPathExtension];
+	if( ![[NSFileManager defaultManager] createDirectoryAtPath: basePath withIntermediateDirectories: NO attributes: @{} error: NULL] )
+		return NO;
+	
+	NSMutableDictionary * mainResDict = [dict[@"WebMainResource"] mutableCopy];
+	if( [mainResDict[@"WebResourceURL"] hasSuffix: @"/"] )
+		mainResDict[@"WebResourceURL"] = @"index";
+	if( ![self createFileForResource: mainResDict basePath: basePath] )
+		return NO;
+	NSArray * subResources = dict[@"WebSubresources"];
+	for( NSDictionary * currResource in subResources )
+	{
+		if( ![self createFileForResource: currResource basePath: basePath] )
+			return NO;
+	}
+	
+	return YES;
+}
+
 @end
